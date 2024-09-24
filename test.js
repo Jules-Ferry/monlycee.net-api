a4b722818ec0a8f01708f771f706753e90b83c9a
Azure <gilleslazure04@gmail.com>
2024-09-24 22:27:04 +0200

    new commit

--- test.js
+++ test.js
@@ -1,10 +1,15 @@
 const MonLyceeAPI = require("./lib/core")
 const config = require("./config.json")
 
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms))
+}
+
 async function main() {
     const session = await MonLyceeAPI.login(config.identifiant, config.password)
     const profile = await session.user.getProfile()
     console.log(profile)
+    await session.logout()
 }
 
 main()
